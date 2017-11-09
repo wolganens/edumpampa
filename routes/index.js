9817a0c6a7bc1de61aad4744c45794d9d4d6038b
Wolgan Ens <ens.wolgan@gmail.com>
2017-11-09 01:33:39 -0200

    Botão limpar e voltar

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ var router = express.Router();
 var indexController = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', indexController.index);
+router.get('/back', function(req, res){	
+	return res.redirect('back');
+})
 module.exports = router;
