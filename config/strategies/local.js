52e807a90833b74cff5542d7671aa8ec83c90e8c
Wolgan Ens <ens.wolgan@gmail.com>
2017-11-29 17:05:33 -0200

    Lint estratégia local de autenticação do passportjs

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -1,29 +1,25 @@
-
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-module.exports = function () {
-  passport.use('local', new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    ((email, password, done) => {
-      User.authenticate(email, password, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+module.exports = () => passport.use('local', new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  ((email, password, done) => {
+    User.authenticate(email, password, (err, user) => {
+      if (err) {
+        return done(err);
+      }
 
-        if (!user) {
-          return done(null, false, { message: 'Email ou senha inválidos.' });
-        }
+      if (!user) {
+        return done(null, false, { message: 'Email ou senha inválidos.' });
+      }
 
-        return done(null, user);
-      });
-    }),
-  ));
-};
+      return done(null, user);
+    });
+  }),
+));
