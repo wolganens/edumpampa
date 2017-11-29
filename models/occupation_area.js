4d2ff6e368b0a91f206c464a0bab9a24e9bc989d
Wolgan Ens <ens.wolgan@gmail.com>
2017-11-29 16:58:12 -0200

    Lint (tab e espaços)  e camelcase model OccupationArea

--- models/occupation_area.js
+++ models/occupation_area.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const occupation_area = new Schema({
+const occupationArea = new Schema({
   name: {
-    	type: String,
-    	unique: true,
-    	maxlength: 200,
+    type: String,
+    unique: true,
+    maxlength: 200,
   },
 });
 
-module.exports = mongoose.model('OccupationArea', occupation_area);
+module.exports = mongoose.model('OccupationArea', occupationArea);
