7e8b7cab6ccaa35150df2ed848135c71a40d6273
Wolgan Ens <ens.wolgan@gmail.com>
2017-11-29 17:03:11 -0200

    Lint model AccessibilityResources

--- models/accessibilityresources.js
+++ models/accessibilityresources.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const accessibility_resources = new Schema({
+const accessibilityResources = new Schema({
   name: {
-    	type: String,
-    	unique: true,
-    	maxlength: 200,
+    type: String,
+    unique: true,
+    maxlength: 200,
   },
 });
 
-module.exports = mongoose.model('AccessibilityResources', accessibility_resources);
+module.exports = mongoose.model('AccessibilityResources', accessibilityResources);
