9bf4118fc1294b0f426adf6a6bbc1269a6da35a3
Wolgan Ens <ens.wolgan@gmail.com>
2017-11-29 17:15:27 -0200

    Lint estratégia google do passportjs

--- config/strategies/google.js
+++ config/strategies/google.js
@@ -6,44 +6,41 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-module.exports = function googleStrategy() {
-  passport.use(new OAuth2Strategy(
-    {
-      clientID: '303825891357-bn5tssc9q4hncnrtippn1c8i744db31h.apps.googleusercontent.com',
-      clientSecret: 'QKeCN2408js1eYIDLLoKlg2z',
-      callbackURL: 'http://edumpampa.herokuapp.com/auth/oauth2callback',
-    },
-    ((accessToken, refreshToken, profile, cb) => {
-      const userData = {
-        name: profile._json.displayName,
-        google_id: profile._json.id,
-        google: profile._json,
-        email: profile._json.emails[0].value,
-      };
-      User.findOne({ email: userData.email }, (err, user) => {
-        console.log(user);
-        if (!user) {
-          User.create(userData, (err, user) => {
-            if (err) {
-              return cb(err, null);
-            }
-            return cb(err, user);
-          });
-        } else {
-          if (!user.google) {
-            user.google = userData.google;
-            user.google_id = userData.google_id;
-            user.save((err, user) => {
-              if (err) {
-                return cb(err, null);
-              }
-              return cb(err, user);
-            });
+module.exports = () => passport.use(new OAuth2Strategy(
+  {
+    clientID: '303825891357-bn5tssc9q4hncnrtippn1c8i744db31h.apps.googleusercontent.com',
+    clientSecret: 'QKeCN2408js1eYIDLLoKlg2z',
+    callbackURL: 'http://edumpampa.herokuapp.com/auth/oauth2callback',
+  },
+  ((accessToken, refreshToken, profile, cb) => {
+    const jsonProfile = profile._json;
+    const userData = {
+      name: jsonProfile.displayName,
+      google_id: jsonProfile.id,
+      google: jsonProfile,
+      email: jsonProfile.emails[0].value,
+    };
+    return User.findOne({ email: userData.email }, (err, user) => {
+      const findUser = user;
+      if (!findUser) {
+        return User.create(userData, (createErr, createdUser) => {
+          if (createErr) {
+            return cb(createErr, null);
           }
-          return cb(err, user);
-        }
-      });
-    }),
-
-  ));
-};
+          return cb(createErr, createdUser);
+        });
+      }
+      if (!findUser.google) {
+        findUser.google = userData.google;
+        findUser.google_id = userData.google_id;
+        return findUser.save((saveErr, savedUser) => {
+          if (saveErr) {
+            return cb(saveErr, null);
+          }
+          return cb(saveErr, savedUser);
+        });
+      }
+      return cb(err, user);
+    });
+  }),
+));
