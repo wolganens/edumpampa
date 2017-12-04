db7cf396a2bcf8ba675a6e598a897887658bdc5c
Wolgan Ens <ens.wolgan@gmail.com>
2017-12-04 13:25:30 -0200

    Rotas de login com google comentadas

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,7 +7,7 @@ const authCtrl = require('../controllers/authentication');
 
 const router = express.Router();
 
-router.post('/signin', authCtrl.signin);
+/*router.post('/signin', authCtrl.signin);
 router.get(
   '/signin-google',
   passport.authenticate('google', {
@@ -22,7 +22,7 @@ router.get(
   (req, res) => {
     res.redirect('/');
   },
-);
+);*/
 router.get('/signout', authCtrl.signout);
 
 module.exports = router;
