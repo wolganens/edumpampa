d04df897cd6c695818d961a58161b283cddf0b7c
Wolgan Ens <ens.wolgan@gmail.com>
2018-03-28 10:20:04 -0300

    Assets via webpack

---
+++ assets/src/index.js
@@ -0,0 +1,3 @@
+require('script-loader!../../node_modules/jquery')
+require('script-loader!../../node_modules/bootstrap-sass')
+require('./sass/app.scss')
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
 var webpack = require('webpack')
+const path = require('path');
 
-module.exports = function(env) {
-    return {
-        entry: "./src/index.js",
-        output: {
-            path: __dirname + "./public/",
-            filename: "app.js"
-        },        
-        module: {
-            loaders: [
-                {test: /\.html$/, loader: 'raw-loader', exclude: /node_modules/},
-                {test: /\.css$/, loader: "style-loader!css-loader", exclude: /node_modules/},
-                {test: /\.scss$/, loader: "style-loader!css-loader!sass-loader", exclude: /node_modules/},
-                {test: /\.jpg($|\?)|\.png($|\?)|\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.gif($|\?)/, loader: 'url-loader'},                
-            ]
-        },
-    }
+module.exports = {    
+    entry: "./assets/src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.js',
+    },        
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['raw-loader'], 
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jpg($|\?)|\.png($|\?)|\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.gif($|\?)/,
+                use: ['url-loader']
+            },
+        ]
+    },
+    mode: 'development'  
 }
